d9dfec5945f01fb6cbd2bfa20ddc2a7644d4e260
Ali Efe GÜR <ali.efe.gur@hotmail.com>
2026-06-27 02:34:19 +0300

    Logging Initialization

--- Fluxion/Fluxion/Core/EntryPoint.h
+++ Fluxion/Fluxion/Core/EntryPoint.h
@@ -6,8 +6,14 @@ extern Fluxion::Application* Fluxion::CreateApplication();
 
 int main(int argc, char** argv)
 {
+    // Initialize logging system
+    Fluxion::Log::Initialize();
+
+    // Create, run & delete application
     const auto app = Fluxion::CreateApplication();
     app->Run();
     delete app;
+
+    // Close program
     return 0;
 }
--- Sandbox/src/main.cpp
+++ Sandbox/src/main.cpp
@@ -5,12 +5,12 @@ class Sandbox : public Fluxion::Application
 public:
     Sandbox()
     {
-
+        FLX_LOG_INFO("Hello World!");
     }
 
     ~Sandbox() override
     {
-
+        FLX_LOG_WARN("Goodbye World!");
     }
 };
 
